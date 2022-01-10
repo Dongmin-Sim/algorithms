40c162b5a68d471ce3a97eeb80ad27fd05126415
codongmin <codongmin@gmail.com>
2022-01-10 17:55:37 +0900

    두 수의 덧셈

--- linked-list/addTwoNumbers.py
+++ linked-list/addTwoNumbers.py
@@ -6,6 +6,8 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         a = self.toList(self.reversedList(l1))
