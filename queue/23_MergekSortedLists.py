21de7de2e2a45aad1bc9620b06d1dda81834a9a5
codongmin <codongmin@gmail.com>
2022-01-27 15:27:49 +0900

    k개 정렬 리스트 병합

---
+++ queue/23_MergekSortedLists.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+import heapq
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        root = result = ListNode(None)
+        
+        heap = []
+        for i in range(len(lists)):
+            heapq.heappush(heap, (lists[i].val, i, lists[i]))
+            
+        while heap:
+            node = heapq.heappop(heap)
+            result.next = node[2]
+            
+            result = result.next
+            
+            if result.next:
+                heapq.heappush(heap, (result.next.val, node[1], result.next))
+            
+        
+        return root.next
