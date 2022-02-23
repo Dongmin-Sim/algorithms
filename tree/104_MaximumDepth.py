bb8a287d73e4d87b5c9704a233d7770b5b089e7a
codongmin <codongmin@gmail.com>
2022-02-23 17:46:18 +0900

    트리 최대 깊이

---
+++ tree/104_MaximumDepth.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        
+        queue = collections.deque([root])
+        max_depth = 0
+        
+        while queue:
+            max_depth += 1
+            for _ in range(len(queue)):
+                cur_node = queue.popleft()
+
+                if cur_node.left:
+                    queue.append(cur_node.left)
+                if cur_node.right:
+                    queue.append(cur_node.right)
+        
+        return max_depth
